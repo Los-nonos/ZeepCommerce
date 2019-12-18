486f889d5831f98754c637b1cfc57601a75c8483
cristianvena18 <cristiandamianvena@gmail.com>
2019-12-18 17:47:26 -0300

    added base

--- src/domain/Entity/User.ts
+++ src/domain/Entity/User.ts
@@ -1,5 +1,9 @@
-class User{
+import { Entity } from 'typeorm';
 
+
+@Entity()
+class User{
+    
 }
 
 export default User;
---
+++ src/domain/Handlers/CreateUserHandler.ts
@@ -0,0 +1,7 @@
+class CreateUserHandler {
+    constructor(){
+        
+    }
+}
+
+export default CreateUserHandler;
