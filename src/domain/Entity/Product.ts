62bf557cfecf5541f57b2a2cd77ecc239ce34833
cristianvena18 <cristiandamianvena@gmail.com>
2019-12-18 23:51:51 -0300

    agrego nuevamente el controller y la entidad de product porque la perdi

---
+++ src/domain/Entity/Product.ts
@@ -0,0 +1,19 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+class Product extends BaseEntity{
+
+    @PrimaryGeneratedColumn()
+    public Id: number;
+
+    @Column()
+    public name: string;
+
+    @Column()
+    public price: number;
+
+    @Column()
+    public description: string;
+}
+
+export default Product;
