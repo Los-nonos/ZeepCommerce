0700f06fd5206646e175c68285d1b96951c99f05
Martin <martinmontanari3@gmail.com>
2020-01-15 20:19:16 -0300

    modificate user routes

--- backend/src/Infraestructure/Router/Router.ts
+++ backend/src/Infraestructure/Router/Router.ts
@@ -56,10 +56,10 @@ class Router {
         });
 
         //user routes
-        this.express.post('/users', this.userController.Create);
-        this.express.get('/users/:id', this.userController.ShowOne);
-        this.express.post('/users/:id', this.userController.Edit);
-        this.express.delete('/users/:id', this.userController.Delete);
+        this.express.post('/apiv1/users', this.userController.Create);
+        this.express.get('/apiv1/users/:id', this.userController.ShowOne);
+        this.express.post('/apiv1/users/:id', this.userController.Edit);
+        this.express.delete('/apiv1/users/:id', this.userController.Delete);
 
         //product routes
         this.express.post('/products', this.productController.Create);
