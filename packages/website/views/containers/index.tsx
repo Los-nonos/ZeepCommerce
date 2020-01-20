79eee41ccc753d2e5fcbabdf36b6c3a62304a886
cristianvena18 <cristiandamianvena@gmail.com>
2020-01-20 18:25:45 -0300

    index page added

---
+++ packages/website/views/containers/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Home extends React.Component {
+  constructor(props: any) {
+      super(props);
+    }
+
+    render(){
+        return(<div>
+            <h1>Hola, esto es un titulo</h1>
+        </div>)
+    }
+}
+
+export default Home;
