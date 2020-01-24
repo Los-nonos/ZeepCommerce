4aa6458b3eff98ab7f301b45dddbb5eec734387f
elparientebrc <ayrtoncravero26@gmail.com>
2020-01-24 20:55:24 -0300

    banch created, for testing

--- packages/api/src/Infraestructure/Interfaces/UserInterfaces/DeleteUserHandlerInterface.ts
+++ packages/api/src/Infraestructure/Interfaces/UserInterfaces/DeleteUserHandlerInterface.ts
@@ -1,7 +1,7 @@
 import DeleteUserCommand from '../../../Domain/Commands/UserCommands/DeleteUserCommand';
 
 interface DeleteUserHandlerInterface {
-  Delete(): Promise<string>;
+  Delete(command: DeleteUserCommand): Promise<string>;
 }
 
 export default DeleteUserHandlerInterface;
