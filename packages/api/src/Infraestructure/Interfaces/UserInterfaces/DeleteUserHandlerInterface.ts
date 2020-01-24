02dae46446e915b4ca7c47243e943a8e15af3048
elparientebrc <ayrtoncravero26@gmail.com>
2020-01-24 20:29:58 -0300

    enseñando git a ayrton

--- packages/api/src/Infraestructure/Interfaces/UserInterfaces/DeleteUserHandlerInterface.ts
+++ packages/api/src/Infraestructure/Interfaces/UserInterfaces/DeleteUserHandlerInterface.ts
@@ -1,7 +1,7 @@
 import DeleteUserCommand from '../../../Domain/Commands/UserCommands/DeleteUserCommand';
 
 interface DeleteUserHandlerInterface {
-  Delete(command: DeleteUserCommand): Promise<string>;
+  Delete(): Promise<string>;
 }
 
 export default DeleteUserHandlerInterface;
