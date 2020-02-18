5c3d258ebffb20a8533be1c17a39ffd429920621
JoaquinMartina <joaquinmartina44@gmail.com>
2020-02-18 15:02:47 -0300

    fix methods in CategoryFindHandler

--- packages/api/src/Application/Handlers/Category/CategoryFindHandler.ts
+++ packages/api/src/Application/Handlers/Category/CategoryFindHandler.ts
@@ -2,11 +2,9 @@ import CategoryFindHandlerInterface from '../../../Infraestructure/Interfaces/Ca
 import Category from '../../../Domain/Entities/Category';
 import { Equal, MoreThanOrEqual } from 'typeorm';
 import CategoryFindCommand from '../../Commands/Category/CategoryFindCommand';
-import { DataBaseError } from '../../../API/Http/Errors/DataBaseError';
 import { injectable, inject } from 'inversify';
 import CategoryRepositoryInterface from '../../../Domain/Interfaces/CategoryRepositoryInterface';
 import TYPES from '../../../Infraestructure/types';
-import { EntityNotFound } from '../../../API/Http/Errors/EntityNotFound';
 
 @injectable()
 class CategoryFindHandler implements CategoryFindHandlerInterface {
@@ -19,29 +17,13 @@ class CategoryFindHandler implements CategoryFindHandlerInterface {
   public HandleFindById = async (command: CategoryFindCommand): Promise<Category> => {
     const category = await this.repository.FindById(command.getId());
 
-    if (!category) {
-      throw new EntityNotFound('Category not found.');
-    }
-
-    try {
-      return category;
-    } catch (error) {
-      throw new DataBaseError(error.message);
-    }
+    return category;
   };
 
   public HandleFindAll = async (command: CategoryFindCommand): Promise<Category[]> => {
-    const categories = await this.repository.FindAll({ where: { Id: MoreThanOrEqual(command.getId()), limit: 10 } });
+    const categories = await this.repository.FindPaginated(command.getId(), 10);
 
-    if (!categories) {
-      throw new EntityNotFound('Categories not found.');
-    }
-
-    try {
-      return categories;
-    } catch (error) {
-      throw new DataBaseError(error.message);
-    }
+    return categories;
   };
 }
 
