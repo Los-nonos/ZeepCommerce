fafb3dc882f8af2902130bfc45253ba5dfd38579
JoaquinMartina <joaquinmartina44@gmail.com>
2020-02-18 15:18:37 -0300

    add implement categoryRoutes in idex.routes

--- packages/api/src/routes/index.routes.ts
+++ packages/api/src/routes/index.routes.ts
@@ -1,6 +1,7 @@
 import userRoutes from './user.routes';
 import productRoutes from './product.routes';
 import authRoutes from './auth.routes';
+import categoryRoutes from './category.routes';
 import { Router } from 'express';
 
 const router = Router();
@@ -10,5 +11,7 @@ router.use(productRoutes);
 router.use(userRoutes);
 
 router.use(authRoutes);
+
+router.use(categoryRoutes);
 
 export default router;
