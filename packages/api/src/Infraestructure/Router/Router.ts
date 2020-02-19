153b64b2d6eabf9647a999c8efcf1364d9281caf
cristianvena18 <cristiandamianvena@gmail.com>
2020-02-19 20:57:14 -0300

    added router

---
+++ packages/api/src/Infraestructure/Router/Router.ts
@@ -0,0 +1,47 @@
+import { Express, Request, Response, NextFunction } from 'express';
+import bodyParser = require('body-parser');
+import { injectable } from 'inversify';
+import cors = require('cors');
+import routes from '../../routes/index.routes';
+
+import container from '../DI/inversify.config';
+import ErrorHandler from '../../Infraestructure/utils/ErrorHandler';
+
+@injectable()
+class Router {
+  private express: Express;
+
+  constructor(express: Express) {
+    this.express = express;
+  }
+
+  public up() {
+    this.middlewares();
+    this.userRoutes();
+    this.catchErrors();
+  }
+
+  catchErrors() {
+    this.express.use((error: Error, _req: Request, res: Response, next: NextFunction) => {
+      if (!error) {
+        next();
+      }
+
+      const errorHandler: ErrorHandler = container.get(ErrorHandler);
+
+      errorHandler.handle(error, res);
+    });
+  }
+
+  middlewares() {
+    this.express.use(cors());
+    this.express.use(bodyParser.urlencoded({ extended: false }));
+    this.express.use(bodyParser.json());
+  }
+
+  private userRoutes() {
+    this.express.use('/apiv1/', routes);
+  }
+}
+
+export default Router;
