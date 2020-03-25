1dcce0a9795dc820173ab4fb594497b689c2bf1d
cristianvena18 <cristiandamianvena@gmail.com>
2020-03-25 13:11:16 -0300

    fix prettier check2

--- views/components/NavBarCustomer.tsx
+++ views/components/NavBarCustomer.tsx
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import Botton from '../components/patterns/atoms/Buttom';
 
 class NavBarCustomer extends Component {
-    render() {
-        return(
-            <div>
-                <header>
-                    <img src="./img/logos/logo.jpg" alt="Logo de la empresa" width="10%"/>
-                    <Botton content='Shopping' color='red' />
-                    <Botton content='Contacto' color='red' />
-                    <Botton content='Aboutus' color='red' />
-                    <Botton content='Login' color='red' />
-                    <Botton content='Sigup' color='red' />
-                </header>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <header>
+          <img src="./img/logos/logo.jpg" alt="Logo de la empresa" width="10%" />
+          <Botton content="Shopping" color="red" />
+          <Botton content="Contacto" color="red" />
+          <Botton content="Aboutus" color="red" />
+          <Botton content="Login" color="red" />
+          <Botton content="Sigup" color="red" />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default NavBarCustomer;
--- views/components/patterns/atoms/Buttom.tsx
+++ views/components/patterns/atoms/Buttom.tsx
@@ -2,14 +2,14 @@ import { Component } from 'react';
 import Button from '@material-ui/core/Button';
 
 class Buttom extends Component<any, any> {
-    render() {
-        const {content} = this.props;
-        return (
-            <Button variant="contained" color='primary'>
-                {content}
-            </Button>
-        )
-    }
+  render() {
+    const { content } = this.props;
+    return (
+      <Button variant="contained" color="primary">
+        {content}
+      </Button>
+    );
+  }
 }
 
 export default Buttom;
