46c7e501fa923c3e33973af01eb3f592fc438624
cristianvena18 <cristiandamianvena@gmail.com>
2020-06-28 17:01:43 -0300

    :bug:

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
+
 export const env = (value, defaultValue = null) => {
+  dotenv.config();
   const result = process.env[value];
   return result ? result : defaultValue;
 }
